4b776728e1bdc6d13bc9ca9256fc7b6b82d2ef5d
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-04-04 11:39:52 +0300

    add more tests

--- tests/Feature/Parser/YouTubeTest.php
+++ tests/Feature/Parser/YouTubeTest.php
@@ -3,10 +3,25 @@
 namespace Tests\Feature\Parser;
 
 use Tests\TestCase;
+use GuzzleHttp\Client;
+use App\Console\Parsers\HttpParser;
 use App\Console\Storages\GoogleStorage;
 
 class YouTubeTest extends TestCase
 {
+    private const RANGE = 'Лист1';
+    private const SPREADSHEET = '1TB855RwBrLonLnknKWXnwvxq2NqxfoDdR0Yp-ko0p68';
+
+    private function fixWhiteSpace( string $word ): string
+    {
+        return str_replace( ' ', '+', trim( $word ) );
+    }
+
+    private function checkHttpData( string $title, string $description, int $position, int $count ): bool
+    {
+        return trim( $title ) !== '' && trim( $description ) !== '' && $position > 0 && $position <= $count;
+    }
+
     public function getDataForCheckGoogle(): array
     {
         return [
@@ -22,8 +37,8 @@ class YouTubeTest extends TestCase
     {
         $this->artisan( 'parser:start' )
             ->expectsQuestion( 'Hi, give me your phrase', 'world without war' )
-            ->expectsQuestion( 'Hi, give me your table list', 'Лист1' )
-            ->expectsQuestion( 'Hi, give me your spreadsheet id', '1TB855RwBrLonLnknKWXnwvxq2NqxfoDdR0Yp-ko0p68' )
+            ->expectsQuestion( 'Hi, give me your table list', self::RANGE )
+            ->expectsQuestion( 'Hi, give me your spreadsheet id', self::SPREADSHEET )
             ->assertSuccessful();
     }
 
@@ -32,25 +47,43 @@ class YouTubeTest extends TestCase
      */
     public function testCheckData( string $word, bool $expected ): void
     {
-        $spreadsheetId = '1TB855RwBrLonLnknKWXnwvxq2NqxfoDdR0Yp-ko0p68';
-        $rangeId = 'Лист1';
         $tableTitles = [ 'URL', 'Title', 'Description', 'Position' ];
 
         $this->artisan( 'parser:start' )
             ->expectsQuestion( 'Hi, give me your phrase', $word )
-            ->expectsQuestion( 'Hi, give me your table list', $rangeId )
-            ->expectsQuestion( 'Hi, give me your spreadsheet id', $spreadsheetId );
+            ->expectsQuestion( 'Hi, give me your table list', self::RANGE )
+            ->expectsQuestion( 'Hi, give me your spreadsheet id', self::SPREADSHEET );
 
         $service = GoogleStorage::getService();
-        $word = str_replace( ' ', '+', trim( $word ) );
+        $word = $this->fixWhiteSpace( $word );
         $firstColumn = [ 'Поисковый запрос', $word ];
 
-        $checkCount = $service->spreadsheets_values->get( $spreadsheetId, $rangeId )->count() > 2;
+        $checkCount = $service->spreadsheets_values->get( self::SPREADSHEET, self::RANGE )->count() > 2;
 
-        $values = $service->spreadsheets_values->get( $spreadsheetId, $rangeId );
+        $values = $service->spreadsheets_values->get( self::SPREADSHEET, self::RANGE );
         $total = array_merge( array_diff( $firstColumn, $values[ 0 ] ), array_diff( $tableTitles, $values[ 1 ] ) );
 
         $this->assertEquals( $expected, empty( $total ) && $checkCount );
+    }
+
+    /**
+     * @dataProvider getDataForCheckGoogle
+     */
+    public function testHttpData( string $word, bool $expected ): void
+    {
+        $parser = new HttpParser();
+        $client = new Client();
+        $data = $parser->getData( $this->fixWhiteSpace( $word ) );
+        $result = true;
+
+        foreach ( $data as $key => $items ) {
+            if ( $key > 1 ) {
+                $response = $client->get( $items[ 0 ] )->getStatusCode() === 200;
+
+                $result = $this->checkHttpData( $items[ 1 ], $items[ 2 ], $items[ 3 ], count( $data ) - 2 );
+            }
+        }
+        $this->assertEquals( $expected, $result && $response );
     }
 
 }
